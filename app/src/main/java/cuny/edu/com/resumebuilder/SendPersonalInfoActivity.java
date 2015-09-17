5141fe072ca77f7d9d8d337a48d62145de39b32d
rragubans <ram_ragubans@yahoo.com>
2015-09-17 08:48:57 -0400

    Update SendPersonalInfoActivity.java

---
+++ app/src/main/java/cuny/edu/com/resumebuilder/SendPersonalInfoActivity.java
@@ -0,0 +1,35 @@
+package cuny.edu.com.resumebuilder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class SendPersonalInfoActivity extends Activity {
+
+    private static final String TAG = "SendPersonalInfoActivit";
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_fragment_1);
+        submitButton = (Button) findViewById(R.id.buttonSendFeedback);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText fullName = (EditText) view.findViewById(R.id.EditTextName);
+                EditText address  = (EditText) view.findViewById(R.id.EditTextAddress);
+                System.out.println("Name " + fullName);
+                Log.v(TAG, "FullName " + fullName);
+                finish();
+            }
+        });
+    }
+}
+
