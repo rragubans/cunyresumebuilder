c7d8328e6ebc12db2dc6941eb01908237f11b286
rragubans <ram_ragubans@yahoo.com>
2015-09-17 08:47:03 -0400

    Update ResumeDataSource.java

---
+++ app/src/main/java/cuny/edu/com/resumebuilder/ResumeDataSource.java
@@ -0,0 +1,49 @@
+package cuny.edu.com.resumebuilder;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class ResumeDataSource {
+
+    // Database fields
+    private SQLiteDatabase database;
+    private SQLLiteHelper dbHelper;
+    private String[] allColumns = { SQLLiteHelper.COLUMN_ID,
+            SQLLiteHelper.COLUMN_NAME };
+
+    public ResumeDataSource(Context context) {
+        dbHelper = new SQLLiteHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public ResumeInformation createResume(String name) {
+        ContentValues values = new ContentValues();
+        values.put(SQLLiteHelper.COLUMN_NAME, name);
+        long insertId = database.insert(SQLLiteHelper.TABLE_RESUME, null, values);
+        Cursor cursor = database.query(SQLLiteHelper.TABLE_RESUME,
+                allColumns, SQLLiteHelper.COLUMN_ID + " = " + insertId, null,
+                null, null, null);
+        cursor.moveToFirst();
+        ResumeInformation newResume = cursorToResume(cursor);
+        cursor.close();
+        return newResume;
+    }
+
+    private ResumeInformation cursorToResume(Cursor cursor) {
+        ResumeInformation resumeInformation = new ResumeInformation();
+        resumeInformation.setId(cursor.getLong(0));
+        resumeInformation.setName(cursor.getString(1));
+        return resumeInformation;
+    }
+
+}
